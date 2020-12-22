e55f843837172443fde24660cdd1d25e3d66586a
Andre Almeida <andre-silva78@hotmail.com>
2020-12-22 15:46:20 -0300

    ++ chat message with firebase implemented, works perfectly

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,24 +11,68 @@ import {
   selectChannelName,
 } from "../features/counter/appSlice";
 import { selectUser } from "../features/counter/userSlice";
+import { useState, useEffect } from "react";
+import db from "./firebase";
+import firebase from "firebase";
 
 const Chat = () => {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [channelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("channels").doc(channelId).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user,
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="chat">
       <ChatHeader channelName={channelName} />
       <div className="chat__messages">
-        <Message />
-        <Message />
-        <Message />
+        {messages.map((message) => (
+          <Message
+            timestamp={message.timestamp}
+            message={message.message}
+            user={message.user}
+          />
+        ))}
       </div>
       <div className="chat__input">
         <AddCircleIcon fontSize="large" />
         <form>
-          <input placeholder={`Mensagem #Raincake`} />
-          <button className="chat__inputButton" type="submit">
+          <input
+            value={input}
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={`Mensagem #${channelName}`}
+          />
+          <button
+            disabled={!channelId}
+            onClick={sendMessage}
+            className="chat__inputButton"
+            type="submit"
+          >
             Enviar
           </button>
         </form>
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,16 +1,18 @@
 import "./Message.css";
 import { Avatar } from "@material-ui/core";
 
-const Message = () => {
+const Message = ({ timestamp, message, user }) => {
   return (
     <div className="message">
-      <Avatar src="https://avatars1.githubusercontent.com/u/61947632?s=400&u=0672b7f3845893b17630bf2e80c554e7f622107a&v=4" />
+      <Avatar src={user.photo} />
       <div className="message__info">
         <h4>
-          aalmeida0
-          <span className="message__timestamp">Esse é um tempoestampa</span>
+          {user.displayName}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
-        <p>Mensagem</p>
+        <p>{message}</p>
       </div>
     </div>
   );
